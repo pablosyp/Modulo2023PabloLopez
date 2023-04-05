e0f930f427b093e3cc2f56a96d89fce39fcdc0a0
PabloLopez <pablosyp@outlook.com>
2023-04-05 20:48:39 -0300

    nota

---
+++ Ejercicio01-Soy mayor de Edad/src/ejercicio01/soy/mayor/de/edad/Ejercicio01SoyMayorDeEdad.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio01.soy.mayor.de.edad;
+
+/**
+ *
+ * @author PabloLopez
+ */
+public class Ejercicio01SoyMayorDeEdad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Variables
+        int edad= 19;
+        System.out.println("--Bienvenido al BAR MOON--");
+        System.out.println("--Antes de ingresar necesito saber su edad--");
+        System.out.println("--Su edad es de " + edad);
+        if (edad > 18){
+            System.out.println("--Tome mas pero no conduzca--");
+        }else {
+        System.out.println("--No puedo beber alchol todavia, tomese una gaseosa");
+        
+    }
+    
+}
+}
