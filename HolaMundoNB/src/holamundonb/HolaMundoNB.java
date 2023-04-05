d261de8953cb9ed1373083ecd98085d22536748b
PabloLopez <pablosyp@outlook.com>
2023-04-05 19:36:31 -0300

    NB

---
+++ HolaMundoNB/src/holamundonb/HolaMundoNB.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package holamundonb;
+
+/**
+ *
+ * @author PabloLopez
+ */
+public class HolaMundoNB {
+    
+    public static void main(String[] args) {
+        //Mostrar en pantalla Hola Mundo
+        System.out.println ("Hola Mundo");
+    }
+    
+}
