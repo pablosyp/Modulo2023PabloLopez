562a029e068ae3879939c00e0db2787b9e11f599
PabloLopez <pablosyp@outlook.com>
2023-04-05 21:25:07 -0300

    v2

---
+++ Ejercicio01-AprueboMayorDeSeis/src/ejercicio01/apruebomayordeseis/Ejercicio01AprueboMayorDeSeis.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio01.apruebomayordeseis;
+
+/**
+ *
+ * @author F4121
+ */
+public class Ejercicio01AprueboMayorDeSeis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Variables
+        int nota= 7;
+        System.out.println("--Examenes--");
+        System.out.println("--Nombre del Alumno: Pablo");
+        System.out.println("--Nota del Alumno " + nota);
+        if (nota >= 6){
+            System.out.println("--Aprobado--");
+        }else {
+        System.out.println("--Nombre del Alumno: Pablo");
+        System.out.println("--Desaprobado--");
+        
+    }
+    
+}
+}
