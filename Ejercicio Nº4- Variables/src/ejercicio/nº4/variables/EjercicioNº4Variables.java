10557ffdaf6ff7b976bfa98e0db8fae9a2ebd695
PabloLopez <pablosyp@outlook.com>
2023-04-07 20:44:57 -0300

    Ej4

---
+++ Ejercicio Nº4- Variables/src/ejercicio/nº4/variables/EjercicioNº4Variables.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.nº4.variables;
+
+/**
+ *
+ * @author PabloLopez
+ */
+public class EjercicioNº4Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Variables
+        int N;
+        double A;
+        char C;
+        double sumar;
+        double restar;
+        //Valor de cada variable
+        N= 10;
+        A= 2.5;
+        C= 50;
+        System.out.println("--Variable N--: " + N);
+        System.out.println("--Variable A--: " + A);
+        System.out.println("--Variable C--: " + C);
+        //Sumar
+        sumar= N + A;
+        System.out.println("--La suma de N + A--: " + sumar);
+        //Restar
+        restar= A - N;
+        System.out.println("--La diferencia de A - N--: " + restar);
+        //Valor numerico de C
+        System.out.println("--Valor numerico del caracter C --: " + C);
+        
+       
+        
+        
+        
+        
+        
+    }
+    
+}
