214a46a4b605a7fe13ab1e1804a97b9b4c52d834
PabloLopez <pablosyp@outlook.com>
2023-04-14 22:22:18 -0300

    ej

---
+++ Ejercicio N°7- MultiplicarX2/src/ejercicio/n/pkg7/multiplicarx2/EjercicioN7MultiplicarX2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio.n.pkg7.multiplicarx2;
+
+import java.util.Scanner;
+
+public class EjercicioN7MultiplicarX2 {
+
+    public static void main(String[] args) {
+        //Variables
+        int num;
+        int resultado;
+        Scanner lector = new Scanner(System.in);
+        
+        System.out.println("Ingredar numero ");
+        num= lector.nextInt();
+        resultado = num * 2;
+        System.out.println("Multiplicado x 2 : " + resultado);
+        
+        
+        
+        
+    }
+    
+}
