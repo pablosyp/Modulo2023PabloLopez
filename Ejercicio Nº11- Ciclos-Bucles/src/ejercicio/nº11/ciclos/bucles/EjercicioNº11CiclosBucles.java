b000af5e0c79daa0fc7460de1621bfc96261b97a
PabloLopez <pablosyp@outlook.com>
2023-04-28 22:06:11 -0300

    Update EjercicioNº11CiclosBucles.java

--- Ejercicio Nº11- Ciclos-Bucles/src/ejercicio/nº11/ciclos/bucles/EjercicioNº11CiclosBucles.java
+++ Ejercicio Nº11- Ciclos-Bucles/src/ejercicio/nº11/ciclos/bucles/EjercicioNº11CiclosBucles.java
@@ -7,12 +7,31 @@ package ejercicio.nº11.ciclos.bucles;
 public class EjercicioNº11CiclosBucles {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        //A FOR
+        System.out.println("Opcion A For");
+        int num1=0;
+        for (int A = 1; A <= 100; A++){
+            System.out.println(+A);
+        }
+        //B WHILE
+        System.out.println("Opcion B While");
+        int num2=1;
+        
+        while( num2 <=100){
+            System.out.println(num2);
+            num2++;
+        }   
+        //DO WHILE
+        System.out.println("Opcion C Do While");
+        int num3=1;
+         do{
+             System.out.println(num3);
+             num3++;
+             }while(num3 <=100);
         
         
         
         
-        
+           
     }
-    
 }
