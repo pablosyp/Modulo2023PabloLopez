899ae3f8cfc76537c78d01b5a3b15a72a768f8e3
PabloLopez <pablosyp@outlook.com>
2023-05-01 19:39:53 -0300

    Ej

--- PiedraPapelTijeta-2/src/piedrapapeltijeta/pkg2/PiedraPapelTijeta2.java
+++ PiedraPapelTijeta-2/src/piedrapapeltijeta/pkg2/PiedraPapelTijeta2.java
@@ -2,24 +2,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package piedrapapeltijera;
+package piedrapapeltijeta.pkg2;
 
 import java.util.*;
 
-public class PiedraPapelTijera {
+public class PiedraPapelTijeta2 {
 
     public static void main(String[] args) {
         //Objetos y Variables
         Scanner leer = new Scanner(System.in);
         String resp1="";
         String resp2="";
+        String resp22="";
+        String resp11="";
         int numerorandom;
+        int numerorandom1;
         String jugador1="R2D";
         String jugador2="";
         Random RD = new Random();
+        
         //numero Random
         numerorandom = RD.nextInt(3)+1;
-        
+        numerorandom1 = RD.nextInt(3)+1;
         //Inicio del Juego
         System.out.println("---------------------------------");
         System.out.print("bienvenidos a Piedra-Papel-Tijera");
@@ -32,9 +36,11 @@ public class PiedraPapelTijera {
         System.out.println("---La P es Piedra---");
         System.out.println("---La L es Papel---");
         System.out.println("---La T es Tijera---");
-        //Pedir al jugador que ongrese la opcion (/n = significa un salto de linea)
-        System.out.print("\nINTRODUCIR 3 OPCIONES P/L/T\n"+ jugador2+ ": ");
+        //Pedir al jugador que ongrese la opcion (\n = significa un salto de linea)
+        System.out.print("\nINTRODUCIR 1er OPCION P/L/T\n"+ jugador2+ ": ");
         resp1=leer.next();
+        System.out.print("\nINTRODUCIR 2da OPCION P/L/T\n"+ jugador2+ ": ");
+        resp11=leer.next();
         //Convertir numeros en letras
         switch(numerorandom){
             case 1:resp2=" P";
@@ -42,8 +48,18 @@ public class PiedraPapelTijera {
             case 2:resp2=" T";
                 break;
             case 3:resp2=" L";
-        }
-        System.out.println("\nLA OPCION DE R2D FUE :" +resp2);
+                break;
+        } 
+                switch(numerorandom1){
+            case 1:resp22=" P";
+                break;
+            case 2:resp22=" T";
+                break;
+            case 3:resp22=" L";
+                break;
+        } 
+        System.out.println("\nLA 1er OPCION DE R2D FUE :" +resp2);
+        System.out.println("\nLA 2da OPCION DE R2D FUE :" +resp22);
         // Logica del juego
         if(resp1.equals("P") && resp2.equals(" P")){
             System.out.println("\nEMPATE!!!\n");
@@ -72,7 +88,36 @@ public class PiedraPapelTijera {
         if(resp1.equals("T") && resp2.equals(" P")){
             System.out.println("\nGANADOR\n" + jugador2);
         }
+        if(resp1.equals("P") && resp22.equals(" P")){
+            System.out.println("\nEMPATE!!!\n");
+        }
+        if(resp1.equals("P") && resp22.equals(" L")){
+            System.out.println("\nGANADOR\n" + jugador1);
+        }
+        if(resp1.equals("P") && resp22.equals(" T")){
+            System.out.println("\nGANADOR\n" + jugador2);
+        }
+        if(resp1.equals("L") && resp22.equals(" L")){
+            System.out.println("\nEMPATE!!!\n");
+        }
+        if(resp1.equals("L") && resp22.equals(" P")){
+            System.out.println("\nGANADOR\n" + jugador1);
+        }
+        if(resp1.equals("L") && resp22.equals(" T")){
+            System.out.println("\nGANADOR\n" + jugador2);
+        }
+        if(resp1.equals("T") && resp22.equals(" T")){
+            System.out.println("\nEMPATE!!!\n");
+        }
+        if(resp1.equals("T") && resp22.equals(" L")){
+            System.out.println("\nGANADOR\n" + jugador1);
+        }
+        if(resp1.equals("T") && resp22.equals(" P")){
+            System.out.println("\nGANADOR\n" + jugador2);
+        }
+        
+
         
     }
 }
-        
+
