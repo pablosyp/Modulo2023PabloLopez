da245238a085731da3aa3e14e3480e626cb07d61
PabloLopez <pablosyp@outlook.com>
2023-05-01 22:49:47 -0300

    Update PiedraPapelTijeta2.java

--- PiedraPapelTijeta-2/src/piedrapapeltijeta/pkg2/PiedraPapelTijeta2.java
+++ PiedraPapelTijeta-2/src/piedrapapeltijeta/pkg2/PiedraPapelTijeta2.java
@@ -12,21 +12,23 @@ public class PiedraPapelTijeta2 {
         //Objetos y Variables
         Scanner leer = new Scanner(System.in);
         String resp1="";
+        String resp11="";
+        String resp111="";
         String resp2="";
         String resp22="";
-        String resp11="";
+        String resp222="";
         int numerorandom;
-        int numerorandom1;
         String jugador1="R2D";
         String jugador2="";
         Random RD = new Random();
+        int punto1=0;
+        int punto2=0;
         
         //numero Random
         numerorandom = RD.nextInt(3)+1;
-        numerorandom1 = RD.nextInt(3)+1;
         //Inicio del Juego
         System.out.println("---------------------------------");
-        System.out.print("bienvenidos a Piedra-Papel-Tijera");
+        System.out.println("Bienvenidos a Piedra-Papel-Tijera");
         System.out.println("---------------------------------");
         //Pedir al tipito su nombre
         System.out.print("Ingrese su nombre : ");
@@ -37,10 +39,13 @@ public class PiedraPapelTijeta2 {
         System.out.println("---La L es Papel---");
         System.out.println("---La T es Tijera---");
         //Pedir al jugador que ongrese la opcion (\n = significa un salto de linea)
+        System.out.println("---------------------------------");
         System.out.print("\nINTRODUCIR 1er OPCION P/L/T\n"+ jugador2+ ": ");
         resp1=leer.next();
         System.out.print("\nINTRODUCIR 2da OPCION P/L/T\n"+ jugador2+ ": ");
         resp11=leer.next();
+        System.out.print("\nINTRODUCIR 3da OPCION P/L/T\n"+ jugador2+ ": ");
+        resp111=leer.next();
         //Convertir numeros en letras
         switch(numerorandom){
             case 1:resp2=" P";
@@ -50,7 +55,7 @@ public class PiedraPapelTijeta2 {
             case 3:resp2=" L";
                 break;
         } 
-                switch(numerorandom1){
+                switch(numerorandom){
             case 1:resp22=" P";
                 break;
             case 2:resp22=" T";
@@ -58,66 +63,159 @@ public class PiedraPapelTijeta2 {
             case 3:resp22=" L";
                 break;
         } 
-        System.out.println("\nLA 1er OPCION DE R2D FUE :" +resp2);
-        System.out.println("\nLA 2da OPCION DE R2D FUE :" +resp22);
-        // Logica del juego
-        if(resp1.equals("P") && resp2.equals(" P")){
-            System.out.println("\nEMPATE!!!\n");
+                switch(numerorandom){
+            case 1:resp222=" P";
+                break;
+            case 2:resp222=" T";
+                break;
+            case 3:resp222=" L";
+                break;
+        } 
+        System.out.println("---------------------------------");
+        System.out.println("---------Jugadas-- 1-- 2-- 3" ); 
+        System.out.println("Jugador 1 :" +jugador1 +" : "+resp2+  " - "+resp22+  " - "+resp222);
+        System.out.println("Jugador 2 :" +jugador2 +" : "+resp1+  " - "+resp11+  " - "+resp111);
+        System.out.println("---------------------------------");
+        //1er jugada
+         if(resp1.equals("P") && resp2.equals(" P")){
+            //System.out.println("\nEMPATE!!!\n");
         }
         if(resp1.equals("P") && resp2.equals(" L")){
-            System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
         }
         if(resp1.equals("P") && resp2.equals(" T")){
-            System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
         }
         if(resp1.equals("L") && resp2.equals(" L")){
-            System.out.println("\nEMPATE!!!\n");
+            //System.out.println("\nEMPATE!!!\n");
+            
         }
         if(resp1.equals("L") && resp2.equals(" P")){
-            System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
         }
         if(resp1.equals("L") && resp2.equals(" T")){
-            System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
         }
         if(resp1.equals("T") && resp2.equals(" T")){
-            System.out.println("\nEMPATE!!!\n");
+            //System.out.println("\nEMPATE!!!\n");
         }
         if(resp1.equals("T") && resp2.equals(" L")){
-            System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
         }
         if(resp1.equals("T") && resp2.equals(" P")){
-            System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
         }
-        if(resp1.equals("P") && resp22.equals(" P")){
-            System.out.println("\nEMPATE!!!\n");
+        //2da Jugada
+         if(resp11.equals("P") && resp22.equals(" P")){
+            //System.out.println("\nEMPATE!!!\n");
         }
-        if(resp1.equals("P") && resp22.equals(" L")){
-            System.out.println("\nGANADOR\n" + jugador1);
+        if(resp11.equals("P") && resp22.equals(" L")){
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
         }
-        if(resp1.equals("P") && resp22.equals(" T")){
-            System.out.println("\nGANADOR\n" + jugador2);
+        if(resp11.equals("P") && resp22.equals(" T")){
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
         }
-        if(resp1.equals("L") && resp22.equals(" L")){
-            System.out.println("\nEMPATE!!!\n");
+        if(resp11.equals("L") && resp22.equals(" L")){
+            //System.out.println("\nEMPATE!!!\n");
+            
         }
-        if(resp1.equals("L") && resp22.equals(" P")){
-            System.out.println("\nGANADOR\n" + jugador1);
+        if(resp11.equals("L") && resp22.equals(" P")){
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
         }
-        if(resp1.equals("L") && resp22.equals(" T")){
-            System.out.println("\nGANADOR\n" + jugador2);
+        if(resp11.equals("L") && resp22.equals(" T")){
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
         }
-        if(resp1.equals("T") && resp22.equals(" T")){
-            System.out.println("\nEMPATE!!!\n");
+        if(resp11.equals("T") && resp22.equals(" T")){
+            //System.out.println("\nEMPATE!!!\n");
         }
-        if(resp1.equals("T") && resp22.equals(" L")){
-            System.out.println("\nGANADOR\n" + jugador1);
+        if(resp11.equals("T") && resp22.equals(" L")){
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
         }
-        if(resp1.equals("T") && resp22.equals(" P")){
-            System.out.println("\nGANADOR\n" + jugador2);
+        if(resp11.equals("T") && resp22.equals(" P")){
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
+        }
+        //3er Jugada
+                if(resp111.equals("P") && resp222.equals(" P")){
+            //System.out.println("\nEMPATE!!!\n");
+        }
+        if(resp111.equals("P") && resp222.equals(" L")){
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
+        }
+        if(resp111.equals("P") && resp222.equals(" T")){
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
+        }
+        if(resp111.equals("L") && resp222.equals(" L")){
+            //System.out.println("\nEMPATE!!!\n");
+            
+        }
+        if(resp111.equals("L") && resp222.equals(" P")){
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
+        }
+        if(resp111.equals("L") && resp222.equals(" T")){
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
+        }
+        if(resp111.equals("T") && resp222.equals(" T")){
+            //System.out.println("\nEMPATE!!!\n");
+        }
+        if(resp111.equals("T") && resp222.equals(" L")){
+            //System.out.println("\nGANADOR\n" + jugador2);
+            //System.out.println(jugador2 + " suma 1 puntos");
+            punto2+=1;
+        }
+        if(resp111.equals("T") && resp222.equals(" P")){
+            //System.out.println("\nGANADOR\n" + jugador1);
+            //System.out.println(jugador1 + " suma 1 puntos");
+            punto1+=1;
         }
         
+        System.out.println("---------------------------------");
+        System.out.println("Totales: ");
+        System.out.println(jugador1 + ": " + punto1 + " puntos");
+        System.out.println(jugador2 + ": " + punto2 + " puntos");
+        System.out.println("---------------------------------");
 
-        
+        if(punto1<punto2){
+               System.out.println("---------------------------------");
+               System.out.println("El ganador es : " + jugador2);
+                       
+               
+               }else{
+               System.out.println("---------------------------------");
+               System.out.println ("El ganador es : " + jugador1);
+               
+               }
     }
 }
 
