5f09d16fab55be0969a1c57390a1d5035e2a8de6
PabloLopez <pablosyp@outlook.com>
2023-05-11 13:22:09 -0300

    ej

--- Ejercicio N°13- ServicioAutomtores/src/ejercicio/n/pkg13/servicioautomtores/EjercicioN13ServicioAutomtores.java
+++ Ejercicio N°13- ServicioAutomtores/src/ejercicio/n/pkg13/servicioautomtores/EjercicioN13ServicioAutomtores.java
@@ -1,42 +1,120 @@
 
 package ejercicio.n.pkg13.servicioautomtores;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class EjercicioN13ServicioAutomtores {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        String nom="";
-        String tel="";
+        String nomCliente="";
         String domVehiculo="";
         String marca="";
-        int data="0";
+        int n;
+        int dni;
+        int telefono;
+        int cant=0;
         
-        String [] servicio = new String [4];
+        Scanner leernomCliente = new Scanner (System.in);
+        Scanner leerdni = new Scanner (System.in);
+        Scanner leertelefono = new Scanner (System.in);
+        Scanner leermarca = new Scanner (System.in);
+        Scanner leerdomVehiculo = new Scanner (System.in);
         
+        int alineacion;
+        int ruedasA=0;
+        int ruedasB=0;
+        int ruedasC=0;
+        int balanceo;
+        int aceite;
+        int cubiertas;
+        
+        Scanner leeralineacion = new Scanner (System.in);
+        Scanner leerbalanceo = new Scanner (System.in);
+        Scanner leeraceite = new Scanner (System.in);
+        Scanner leercubiertas = new Scanner (System.in);
+        
+        Scanner leer = new Scanner (System.in);
+                
+        System.out.println("Nombre del Cliente : ");
+	nomCliente = leernomCliente.next();
+        System.out.println("DNI : ");
+	dni = leerdni.nextInt();
+        System.out.println("Telefono : ");
+	telefono = leertelefono.nextInt();
+        System.out.println("Marca : ");
+	marca = leermarca.next();
+        System.out.println("Dominio del Vehiculo : ");
+	domVehiculo = leerdomVehiculo.next();
+        
+        System.out.print("Servicio alineacion 1=si / 0=no: ");
+        alineacion = leeralineacion.nextInt();
+            if (alineacion ==1){
+                    cant+=1;
+                    System.out.println ("Cantidad ruedas a alinear: ");
+                    ruedasA = leer.nextInt();
+                    }
+        System.out.print("Servicio balanceo 1=si / 0=no: ");
+        balanceo = leerbalanceo.nextInt();
+            if (balanceo ==1){
+                    cant+=1;
+                    System.out.println ("Cantidad ruedas a balancear: ");
+                    ruedasB = leer.nextInt();
+                    }  
+        System.out.print("Servicio cambio de Aceite 1=si / 0=no: ");
+        aceite = leeraceite.nextInt();
+            if (aceite ==1){
+                    cant+=1;
+                    }
+        System.out.print("Servicio cambio de cubiertas 1=si / 0=no: ");
+        cubiertas = leercubiertas.nextInt();
+            if (cubiertas ==1){
+                    cant+=1;
+                    System.out.println ("Cantidad cubiertas a cambiar: ");
+                    ruedasC = leer.nextInt();
+                    } 
+       
+            
+        //Imprimir ticket
+        System.out.println("\n==================================");
+        System.out.println("======== Bienvenido ==============");
+        System.out.println("Centro de Servicios Automotor CFP N°36\nZavaleta 204, C1437EYF, CABA");
+                Random orden = new Random ();
+                n=orden.nextInt(100)+1;
+        
+        System.out.println("=====Nº ORDEN: " + n + "===========================");
+        System.out.println("Nombre del Cliente: " + nomCliente);
+        System.out.println("DNI del cliente: " + dni);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Marca del Vehiculo: " + marca);
+        System.out.println("Dominio del Vehiculo: " + domVehiculo);
+        System.out.println("==================================");
+        System.out.println("Cantidad de servicios: " + cant);
+        
+        String servicio[]=new String[4];
+       
         servicio [0]="alineacion";
         servicio [1]="balanceo";
         servicio [2]="aceite";
         servicio [3]="cubierta";
         
-        Scanner leer = new Scanner(System.in);
-         
-        System.out.print("Ingrese su nombre : " );
-        nom = leer.next();
-        System.out.print("Ingrese su telefono : " );
-        tel = leer.next();
-        System.out.print("Ingrese Dominio del vehiculo : " );
-        domVehiculo = leer.next();
-        System.out.print("Marca : ");
-        marca = leer.next();
-        
-        for(int i=0; i<servicio.length; i++){
-            do {
-                    System.out.println("Desea agregar " +servicio[i]+ " ?. "); 
-                    System.out.print("Escriba 1 para si, 2 para no : "); 
-            }}}}
+        System.out.println("Los servicios son : " );
+            if (alineacion ==1){
+                System.out.println (""+ servicio [0] + " / " + "Cantidad de ruedas :" + ruedasA );
             }
+            if (balanceo ==1){
+            System.out.println (""+ servicio [1] + " / " + "Cantidad de ruedas :"  + ruedasB );
+            }
+            if (aceite ==1){
+                System.out.println (""+ "Cambio de "+ servicio [2] );
+            }
+            if (cubiertas ==1){
+                System.out.println (""+ "Cambio de "+ servicio [3] + " / " + "Cantidad de ruedas :"  + ruedasC);
+            }
+        System.out.println("==================================");
+        
+        
     }
 }
 
---
+++ Ejercicio N°14- Consesionaria-/src/ejercicio/n/pkg14/consesionaria/EjercicioN14Consesionaria.java
@@ -0,0 +1,219 @@
+
+package ejercicio.n.pkg14.consesionaria;
+
+import java.util.*;
+
+public class EjercicioN14Consesionaria {
+
+    public static void main(String[] args) {
+        // // Variables
+     String nombrevendedor = "";
+     String nombrecliente = "";
+     String marca = "";
+     String modelo = "";
+     String gama = "";
+     String usado = "";
+     String dominio = "";
+     int dni;
+     int tel;
+     int importe;
+     
+     int opcion;
+     int numero;
+     int imptotalefect;
+     int imptotal70;
+     int imptotal80;
+     int imptotal100;
+     int importefinal;
+     int importefinal70;
+     int importefinal80;
+     int importefinal100;
+     int cuota120;
+     int cuota85;
+     int cuota240;
+     
+    
+        Scanner leernombrevendedor = new Scanner(System.in);
+        Scanner leernombrecliente = new Scanner(System.in);
+        Scanner leerdni = new Scanner(System.in);
+        Scanner leertel = new Scanner(System.in);
+        Scanner leermarca = new Scanner(System.in);
+        Scanner leermodelo = new Scanner(System.in);
+        Scanner leergama = new Scanner(System.in);
+        Scanner leerimporte = new Scanner(System.in);
+        Scanner leerusado = new Scanner(System.in);
+        Scanner leerdominio = new Scanner(System.in);
+        
+        Scanner leeropcion = new Scanner(System.in);
+        Scanner leerimptotalefect = new Scanner(System.in);
+        Scanner leerimptotal70 = new Scanner(System.in);
+        Scanner leerimptotal80 = new Scanner(System.in);
+        Scanner leerimptotal100 = new Scanner(System.in);
+        Scanner leerimportefinal = new Scanner(System.in);
+        Scanner leerimportefinal70 = new Scanner(System.in);
+        Scanner leerimportefinal80 = new Scanner(System.in);
+        Scanner leerimportefinal100 = new Scanner(System.in);
+        Scanner leercuota120 = new Scanner(System.in);
+        Scanner leercuota85 = new Scanner(System.in);
+        Scanner leercuota240 = new Scanner(System.in);
+    
+        System.out.println("=================================");
+        System.out.println("          CONCESIONARIA          ");
+        System.out.println("         RENAULT JOSECAR         ");
+        System.out.println(" Venta de 0KM - Planes de Ahorro ");        
+        System.out.println("=================================");
+        
+        System.out.println("Ingrese el Nombre del Vendedor: ");
+	nombrevendedor = leernombrevendedor.next();
+        
+        System.out.println("Ingrese el Nombre del Cliente: ");
+	nombrecliente = leernombrecliente.next();
+        
+        System.out.println("Ingrese el DNI: ");
+	dni = leerdni.nextInt();
+        
+        System.out.println("Ingrese el Telefono: ");
+	tel = leertel.nextInt();
+        
+        System.out.println("Ingrese la Marca del Automotor: ");
+	marca = leermarca.next();
+        
+        System.out.println("Ingrese el Modelo del Automotor: ");
+	modelo = leermodelo.next();
+        
+        System.out.println("Ingrese la Gama del Automotor: ");
+	gama = leergama.next();
+        
+        System.out.println("------Ingrese el Precio Total------");
+	importe = leerimporte.nextInt();
+        
+        System.out.println("Entrega Vehiculo Usado: " );
+	usado = leerusado.next();
+        
+        System.out.println("Dominio del Vehiculo Usado: " );
+	dominio = leerdominio.next();
+        
+        System.out.println("-----------------------------------");
+        System.out.println("---------Opciones de Pago:---------");
+        System.out.println("-----------------------------------");
+        System.out.println("-1- 100% en Efectivo---------------");
+        System.out.println("-2- 70% Finaniado y 30% en efectivo");
+        System.out.println("-3- 80% Finaniado y 20% en efectivo");
+        System.out.println("-4- 100% Financiado");
+        System.out.println("---------------------------------");
+        System.out.println("Ingrese una opion de Pago");
+	opcion = leeropcion.nextInt();
+    
+        
+        
+       //logica del switch- con tarjeta pedir numero
+        switch (opcion){
+            case 1:
+        System.out.println("En Efectivo paga el 100% en una Cuota"); 
+        imptotalefect = importe;
+        System.out.println("El Total a pagar es:" + imptotalefect); 
+        break;
+            
+            case 2:
+        System.out.println("Con 70% Finaniado y 30% en efectivo");         
+        imptotal70 = +importe *70 / 100;
+        importefinal70 = importe + imptotal70;
+        System.out.println("El Total a pagar en 120 cuotas es:" + importefinal70);
+        cuota120 = importefinal70 / 120;
+        System.out.println("El Valor a pagar por cuota es:" + cuota120);
+        break;
+        
+            case 3:
+        System.out.println("Con 80% Finaniado y 20% en efectivo");                
+        imptotal80 = +importe *80 / 100;
+        importefinal80 = importe + imptotal80;
+        System.out.println("El Total a pagar en 85 cuotas es:" + importefinal80);
+        cuota85 = importefinal80 / 85;
+        System.out.println("El Valor a pagar por cuota es:" + cuota85);
+        break;
+            
+            case 4:
+        System.out.println("100% Financiado");                
+        imptotal100 = +importe *100 / 100;
+        importefinal100 = importe + imptotal100;
+        System.out.println("El Total a pagar en 240 cuotas es:" + importefinal100);
+        cuota240 = importefinal100 / 240;
+        System.out.println("El Valor a pagar por cuota es:" + cuota240);
+        break;
+        
+            default:
+               System.out.println("Elija del 1 al 4 la opcion deseada");
+           break;
+        }
+        System.out.println("=================================");
+        System.out.println("          CONCESIONARIA          ");
+        System.out.println("         RENAULT JOSECAR         ");
+        System.out.println(" Venta de 0KM - Planes de Ahorro ");        
+        System.out.println("=================================");
+        System.out.println("*********PRESUPUESTO*************");
+        System.out.println("=================================");
+        System.out.println("Atendido por el Vendedor: " + nombrevendedor);
+        System.out.println("=================================");
+        System.out.println("-------DATOS DEL COMPRADOR------ ");   
+        System.out.println("Nombre: " + nombrecliente);
+        System.out.println("DNI: " + dni);
+        System.out.println("Telefono: " + tel);
+        System.out.println("=================================");
+        System.out.println("-------DATOS DEL AUTOMOTOR------ ");
+        System.out.println("MARCA: " + marca);
+        System.out.println("MODELO: " + modelo);
+        System.out.println("GAMA: " + gama);
+        System.out.println("PRECIO: " + importe);
+        System.out.println("=================================");
+        System.out.println("------PLAN de PAGO ELEGIDO------ ");
+        switch (opcion){
+            case 1:
+        System.out.println("En Efectivo paga el 100% en una Cuota"); 
+        imptotalefect = importe;
+        System.out.println("El Total a pagar es:" + imptotalefect); 
+        break;
+            
+            case 2:
+        System.out.println("Con 70% Finaniado y 30% en efectivo");         
+        imptotal70 = +importe *70 / 100;
+        importefinal70 = importe + imptotal70;
+        System.out.println("El Total a pagar en 120 cuotas es:" + importefinal70);
+        cuota120 = importefinal70 / 120;
+        System.out.println("El Valor a pagar por cuota es:" + cuota120);
+        break;
+        
+            case 3:
+        System.out.println("Con 80% Finaniado y 20% en efectivo");                
+        imptotal80 = +importe *80 / 100;
+        importefinal80 = importe + imptotal80;
+        System.out.println("El Total a pagar en 85 cuotas es:" + importefinal80);
+        cuota85 = importefinal80 / 85;
+        System.out.println("El Valor a pagar por cuota es:" + cuota85);
+        break;
+            
+            case 4:
+        System.out.println("100% Financiado");                
+        imptotal100 = +importe *100 / 100;
+        importefinal100 = importe + imptotal100;
+        System.out.println("El Total a pagar en 240 cuotas es:" + importefinal100);
+        cuota240 = importefinal100 / 240;
+        System.out.println("El Valor a pagar por cuota es:" + cuota240);
+        break;
+            default:
+               System.out.println("Elija del 1 al 4 la opcion deseada");
+           break;
+        }
+        System.out.println("=================================");
+	System.out.println("Entrega Vehiculo Usado: " + usado);
+	System.out.println("Dominio del Vehiculo Usado: " + dominio);
+	System.out.println("=================================");
+        System.out.println("-GRACIAS POR CONFIAR EN NOSOTROS-");        
+        System.out.println("=================================");
+        
+        
+        
+        
+    }
+    
+}
+
