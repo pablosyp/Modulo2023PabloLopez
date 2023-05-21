de56be6899ffd3042f2982496440cbc6a50fbb1a
PabloLopez <pablosyp@outlook.com>
2023-05-21 14:41:46 -0300

    Ej

---
+++ Trabajo Practico - Seguro Automotor/src/trabajo/practico/seguro/automotor/TrabajoPracticoSeguroAutomotor.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabajo.practico.seguro.automotor;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class TrabajoPracticoSeguroAutomotor {
+
+    public static void main(String[] args) {
+        //Variables
+        /*String nombre="";
+        String apellido="";
+        String telefono;
+        String mail="";
+        */       
+        int fecha=2023;
+        //Logo
+        ImageIcon Icon = new ImageIcon("src/Imagenes/logo.png");
+        JOptionPane.showMessageDialog(null,Icon);
+        //Solicitud de datos
+        /*String nombre = JOptionPane.showInputDialog(null,"Nombre del Cliente ","Poliza",JOptionPane.QUESTION_MESSAGE);
+        String apellido = JOptionPane.showInputDialog(null,"Apellido del Cliente ","Poliza",JOptionPane.QUESTION_MESSAGE);
+        String telefono = JOptionPane.showInputDialog(null,"Telefono ","Poliza",JOptionPane.QUESTION_MESSAGE);
+        String mail = JOptionPane.showInputDialog(null,"Mail ","Poliza",JOptionPane.QUESTION_MESSAGE);
+        
+        //Modelo
+        String modelo = JOptionPane.showInputDialog(null,"Ingrese Modelo :","Poliza",JOptionPane.QUESTION_MESSAGE);
+
+        //Dominio
+        String dominio = JOptionPane.showInputDialog(null,"Ingrese Dominio :","Poliza",JOptionPane.QUESTION_MESSAGE);
+*/
+        //Año de Antiguedad
+        int antig = Integer.parseInt(JOptionPane.showInputDialog(null,"Ingrese Año de Antiguedad ","Poliza",JOptionPane.QUESTION_MESSAGE));
+        int resultado = (fecha - antig);
+      
+        JOptionPane.showMessageDialog(null,"Antiguedad"+ "\n" + resultado + "\n");
+        
+        if (resultado <= 5){
+            JOptionPane.showMessageDialog(null,"Pagara un Extra de $500");
+        }else if ((resultado >= 5) && (resultado < 10)){
+            JOptionPane.showMessageDialog(null,"Pagara un Extra de $1000");
+        }else if ((resultado >= 10) && (resultado < 15)){
+            JOptionPane.showMessageDialog(null,"Pagara un Extra de $1500");
+        }else if ((resultado >= 15) && (resultado < 20)){
+            JOptionPane.showMessageDialog(null,"Pagara un Extra de $2000");
+        }else if (resultado >= 20){
+            JOptionPane.showMessageDialog(null,"No se Asegura el Auto");
+        
+        
+    }
+    
+    }
+}
+    
