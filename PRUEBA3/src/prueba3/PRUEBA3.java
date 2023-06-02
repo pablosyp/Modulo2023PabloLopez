fd95cfd2171c18b82184f9b419e2ef955c45a495
PabloLopez <pablosyp@outlook.com>
2023-06-01 21:42:45 -0300

    Update PRUEBA3.java

--- PRUEBA3/src/prueba3/PRUEBA3.java
+++ PRUEBA3/src/prueba3/PRUEBA3.java
@@ -4,10 +4,8 @@
  */
 package prueba3;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.ImageIcon;
-import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 
 public class PRUEBA3 {
@@ -17,23 +15,24 @@ public class PRUEBA3 {
         ImageIcon Icon2 = new ImageIcon("src/Imagenes/Logo.png");
         String cobertura="";
         String coberturaAD="";
+        String coberADD []= {"Granizo","Llantas Deportivas","Asistencia Mecanica","Localizador GPS"};
         String coberturaAD1="Granizo";
         String coberturaAD2="Llantas Deportivas";
         String coberturaAD3="Asistencia Mecanica";
         String coberturaAD4="Localizador GPS";
         int contador=0;
         int coberAD=0;
-/*
+
             Scanner sc =new Scanner(System.in);
             int opciones;
             do{
-            System.out.println("Eliga la opcion: ");
-            JOptionPane.showOptionDialog(null,
-            "Seleccione cobertura Adicional : ",
-            "Poliza",
-            JOptionPane.YES_NO_CANCEL_OPTION,
-            JOptionPane.QUESTION_MESSAGE,Icon2,
-            new Object[]{"Granizo","NO"},"");
+                System.out.println("Eliga la opcion: ");
+                JOptionPane.showOptionDialog(null,
+                "Seleccione cobertura Adicional : ",
+                "Poliza",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,Icon2,
+                new Object[]{"Granizo","NO"},"");
             
             JOptionPane.showOptionDialog(null,
             "Seleccione la cobertura Adicional : ",
@@ -54,7 +53,7 @@ public class PRUEBA3 {
             
             switch(opciones){
             case 0: JOptionPane.showMessageDialog(null,"Selecciono : Granizo");
-            coberturaAD1 ="Granizo";
+            coberADD = [0];
             contador+=1;
             break;
             default:  
@@ -92,56 +91,16 @@ public class PRUEBA3 {
                JOptionPane.showMessageDialog(null,"Suma de adicional : " + 600);
                coberAD = 600;
 }
-            /*
+            
             JOptionPane.showOptionDialog(null,
             "\n Valor cobertura :"+coberAD+"\n VALOR DE LA PRIMA TOTAL :"+coberturaAD1+"\n VALOR DE LA PRIMA TOTAL :"+coberturaAD2+"\n VALOR DE LA PRIMA TOTAL :"+coberturaAD3+"\n VALOR DE LA PRIMA TOTAL :"+coberturaAD4,
             "Poliza",
             JOptionPane.YES_NO_CANCEL_OPTION,
             JOptionPane.PLAIN_MESSAGE, Icon2,
             new Object[]{"IMPRIMIR POLIZA"},"");
-*/
+
             
-            
-                       // ADICIONALES ***
-            boolean nuevoAdicional= true;
-            int totalPoliza=0;
-            int cantidadAdicionales = 0;
-                       
-            String adicionales[] = {"Granizo", "Llantas deportivas", "Asistencia mecanica", "Localizador GPS", "Ninguna"};
-              ArrayList adicionalElegido = new ArrayList();   
-              
-            JOptionPane.showMessageDialog(null, "*Granizo \n *Llantas Deportivas \n *Asistencia Mecánica \n *Localizador GPS ",
-                   "SERVICIOS ADICIONALES", JOptionPane.INFORMATION_MESSAGE, Icon2);  
-            JOptionPane.showMessageDialog(null, "*Cada adicional cuesta $300, \n pero si elige 2, \n el segundo ES GRATIS!",
-                   "SERVICIOS ADICIONALES", JOptionPane.INFORMATION_MESSAGE, Icon2);  
-            
-            while(nuevoAdicional== true){    
-                String adicional= (String) JOptionPane.showInputDialog(null,
-                               "Seleccionar adicionales",
-                               "ADICIONALES",
-                               JOptionPane.QUESTION_MESSAGE,
-                               null,
-                               adicionales,
-                               adicionales[0]);
-                //agregamos a la lista 
-                adicionalElegido.add(adicional);
-               System.out.println(adicionalElegido);
-               int agregarServicio = JOptionPane.showOptionDialog(null, "Eligue una opcion", "BLA",
-                                    JOptionPane.YES_NO_CANCEL_OPTION,
-                                    JOptionPane.INFORMATION_MESSAGE, null,
-                                    new Object[]{"AGREGAR", "NO, GRACIAS"}, "AGREGAR");                
-               if(agregarServicio == 1){
-                    nuevoAdicional=false;
-               }                    
-            }    
-            for(int i=0;i<adicionalElegido.size();i++){
-                System.out.println(adicionalElegido.get(i));
-                totalPoliza +=300;
-                cantidadAdicionales +=1;
-            }
-                if( cantidadAdicionales % 2 == 0){
-                    totalPoliza-=300;
-                }
+    
     }
 }
 
